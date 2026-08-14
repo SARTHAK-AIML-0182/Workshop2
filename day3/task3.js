481d33f5f13ea0f0bee228b2b677b2d313c69fd2
SARTHAK-AIML-0182 <itzsarthakhere@gmail.com>
2026-08-14 11:23:37 +0530

    task3 day3

---
+++ day3/task3.js
@@ -0,0 +1,8 @@
+var Maths=80;
+var English=75;
+var Science=90;
+function CalculatePercentage(Maths,English,Science){
+    return (Maths + English + Science) / 3;
+
+}
+console.log("Percentage:", CalculatePercentage(Maths, English, Science)+ " %");
