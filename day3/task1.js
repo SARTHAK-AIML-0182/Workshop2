366d94c462ba08721ded6778a1ee75b7b8ed4ae2
SARTHAK-AIML-0182 <itzsarthakhere@gmail.com>
2026-08-14 11:22:54 +0530

    task1 day3

---
+++ day3/task1.js
@@ -0,0 +1,9 @@
+let name = "Sarthak";
+let id = 101;
+let branch = "AIML";
+let department = "Artificial Intelligence and Machine Learning";
+
+console.log("Name:", name);
+console.log("ID:", id);
+console.log("Branch:", branch);
+console.log("Department:", department);
